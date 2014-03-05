ca7c5509419b5a6d8b53ed68f0fb230dfb46121d
Sara Thorup <thorups@gmail.com>
2014-03-05 13:07:31 -0800

    tutorial: specifying the icon

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -1,10 +1,17 @@
+//Toolbar Button Tutorial
 var widgets = require("sdk/widget");
 var tabs = require("sdk/tabs");
+var self = require("sdk/self");
+
 var widget = widgets.Widget({
-	id: "jquery-link",
-	label: "jQuery website",
-	contentURL: "http://www.jquery.com/favicon.ico",
-	onClick: function() {
-		tabs.open("http://www.jquery.com/");
-	}
+  id: "mozilla-link",
+  label: "Mozilla website",
+  contentURL: "http://www.mozilla.org/favicon.ico",
+  contentScriptFile: self.data.url("click-listener.js")
+});
+widget.port.on("left-click", function(){
+  console.log("left-click");
+});
+widget.port.on("right-click", function(){
+  console.log("right-click");
 });
