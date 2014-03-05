d41739222dd83f46d1b233760fde4574eba934cc
Sara Thorup <thorups@gmail.com>
2014-03-05 13:52:22 -0800

    tutorial: panel popup

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -1,17 +1,37 @@
-//Toolbar Button Tutorial
-var widgets = require("sdk/widget");
-var tabs = require("sdk/tabs");
-var self = require("sdk/self");
+//Display a popup panel
+var data = require("sdk/self").data;
+//constrct a panel, loading its content from the text.entry-html
+//file in the "data" directory and loading the get-text.js script into its
 
-var widget = widgets.Widget({
-  id: "mozilla-link",
-  label: "Mozilla website",
-  contentURL: "http://www.mozilla.org/favicon.ico",
-  contentScriptFile: self.data.url("click-listener.js")
+var text_entry = require("sdk/panel").Panel({
+	width: 212,
+	height: 200,
+	contentURL: data.url("text-entry.html"),
+	contentScriptFile: data.url("get-text.js")
 });
-widget.port.on("left-click", function(){
-  console.log("left-click");
+
+//create a widget, and attach the panel to it, so the panel its
+//shown when the user clicks the widget.
+
+require("sdk/widget").Widget({
+	label: "Text entry",
+	id: "text-entry",
+	contentURL: "http://www.mozilla.org/favicon.ico",
+	panel: text_entry
 });
-widget.port.on("right-click", function(){
-  console.log("right-click");
+
+//when the panel is displayed, it generates an event called 'show'
+//we will listen for that event and then send our own show event to the panel's 
+//scirpt so that the script can prepare the panel for displayed
+text_entry.on("show", function() {
+	text_entry.port.emit("show");
+});
+
+//listen for mesages called 'text-entered' coming from the content script
+//the message payload is the ext the user text-entered
+//In this implementation, we'll just log the text to the console
+
+text_entry.port.on("text-entered", function(text) {
+	console.log(text);
+	text_entry.hide();
 });
