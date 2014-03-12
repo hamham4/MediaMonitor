39b3fdad611cd6846dd8f36eca395d9b70dd00e2
Sara Thorup <thorups@gmail.com>
2014-03-12 16:20:27 -0700

    second attempt at using timer

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -3,18 +3,18 @@
 //needed to modify tabs in firefox extension
 require("sdk/tabs").on("ready", onNewTab);
 
-var TAB; 
 function onNewTab(tab) {
-	TAB = tab;
+//sets which function is called after x ms
 
-	//sets which function is called after x ms
-	setTimeout(onTimeOut, 20);
+setTimeout(function() {
+		onTimeOut(tab);
+	},
+	200);
 }
 
-function onTimeOut(){
-	TAB.attach({
-            contentScript: 'document.body.innerHTML = ' +
-                 ' "<h1>Browsing time has expired</h1>";'
-     });
+function onTimeOut(tab){
+	tab.attach({
+	            contentScript: 'document.body.innerHTML = ' +
+	                 ' "<h1>Browsing time has expired</h1>";'
+	     });
 }
-
