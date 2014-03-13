4e658a32ee945dd0847fefb9a1845c3712256f9b
Sara Thorup <thorups@gmail.com>
2014-03-12 19:07:58 -0700

    working timer

--- my-addon/lib/main.js
+++ my-addon/lib/main.js
@@ -1,15 +1,30 @@
 //Practice with time
 
 //needed to modify tabs in firefox extension
-require("sdk/tabs").on("ready", onNewTab);
+var tabs = require("sdk/tabs");
 var tmr = require("sdk/timers");
+var windows = require("sdk/windows").browserWindows;
 
-	tmr.setTimeout(function() {
-		onTimeOut(tab);
-	},
-        10000);
+//pretend that we are readng and updating this from file
+var elapsedTime = 0;
+
+	
+var myTimer = tmr.setInterval(timeCheck, 10000);
+
+function timeCheck(){
+	if (elapsedTime == 3) {
+
+		tmr.clearInterval(myTimer);
+		windows.activeWindow.close(function() {
+ 	 		console.log("The active window was closed");
+		});
+	}
+	elapsedTime = elapsedTime + 1;
+}
 
 
+
+//will probably not need - remove later
 function onNewTab(tab) {
 //sets which function is called after x ms
 	tab.attach({
@@ -17,12 +32,17 @@ function onNewTab(tab) {
     });
 	
 
+	/*tmr.setTimeout(function() {
+		onTimeOut(tab);
+	},
+        10000); */
 
 	tab.attach({
             contentScript: "if (document.body) document.body.style.border = '5px solid red';"
     });
 }
 
+//will probably not need - remove later
 function onTimeOut(tab){
 	tab.attach({
 	            contentScript: 'document.body.innerHTML = ' +
