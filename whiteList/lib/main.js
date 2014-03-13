e6bb6542cbe95a77c856d85fa382f7f8024e1cdf
Sara Thorup <thorups@gmail.com>
2014-03-12 19:55:02 -0700

    added practice panel for displaying time

--- whiteList/lib/main.js
+++ whiteList/lib/main.js
@@ -1,6 +1,23 @@
 require("sdk/tabs").on("ready", blockOrAllowURL);
 var tmr = require("sdk/timers");
 var windows = require("sdk/windows").browserWindows;
+var data = require("sdk/self").data;
+var timeRemainingPanel = require("sdk/panel").Panel({
+  width: 200,
+  height: 50,
+  contentURL: data.url("panelText.html"),
+});
+
+require("sdk/widget").Widget({
+  label: "timeRemaining",
+  id: "timeRemaining",
+  contentURL: "http://www.mozilla.org/favicon.ico",
+  panel: timeRemainingPanel
+});
+
+timeRemainingPanel.on("show", function() {
+  timeRemainingPanel.port.emit("show");
+});
 
 //pretend that we are readng and updating this from file
 var elapsedTime = 0;
