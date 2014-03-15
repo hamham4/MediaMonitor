3eddc1b8b5c3d801d6f77d28709a4b380bcc6429
Sara Thorup <thorups@gmail.com>
2014-03-14 19:45:36 -0700

    reverting to working whitelist-timer version

--- whiteList/lib/main.js
+++ whiteList/lib/main.js
@@ -1,75 +1,14 @@
 require("sdk/tabs").on("ready", blockOrAllowURL);
 var tmr = require("sdk/timers");
 var windows = require("sdk/windows").browserWindows;
-var data = require("sdk/self").data;
-
-///////////////////////////////////////////////////
-//Read and write remaining time to file          //
-///////////////////////////////////////////////////
-
-//if we had the path for the panelText.html file, then we coudl modify it
-//var filename = null;
-
-
-//this could be used to modify the file if we had the path
-//writeTextToFile("did it work?!?!?", filename);
-
-function writeTextToFile(text, filename) {
-  var fileIO = require("sdk/io/file");
-  var TextWriter = fileIO.open(filename, "w");
-  if (!TextWriter.closed) {
-    TextWriter.write(text);
-    TextWriter.close();
-  }
-}
-
-function readTextFromFile(filename) {
-  var fileIO = require("sdk/io/file");
-  var text = null;
-  if (fileIO.exists(filename)) {
-    var TextReader = fileIO.open(filename, "r");
-    if (!TextReader.closed) {
-      text = TextReader.read();
-      TextReader.close();
-    }
-  }
-  return text;
-}
-
-///////////////////////////////////////////////////
-//Create a panel to display remaining time       //
-///////////////////////////////////////////////////
-var timeRemainingPanel = require("sdk/panel").Panel({
-  width: 200,
-  height: 50,
-  contentURL: data.url('panelText.html'),
-});
-
-require("sdk/widget").Widget({
-  label: "timeRemaining",
-  id: "timeRemaining",
-  contentURL: "http://www.mozilla.org/favicon.ico",
-  panel: timeRemainingPanel
-});
-
-timeRemainingPanel.on("show", function() {
-  timeRemainingPanel.port.emit("show");
-});
-
-///////////////////////////////////////////////////////
-//Close all firefox windows after elapsed time       //
-///////////////////////////////////////////////////////
-
 
 //pretend that we are readng and updating this from file
-//in minutes
 var elapsedTime = 0;
-var allotedTime = 1;
 
-var myTimer = tmr.setInterval(timeCheck, 60000);
+var myTimer = tmr.setInterval(timeCheck, 10000);
 
 function timeCheck(){
-  if (elapsedTime == allottedTime) {
+  if (elapsedTime == 3) {
 
     tmr.clearInterval(myTimer);
     windows.activeWindow.close(function() {
@@ -82,9 +21,6 @@ function timeCheck(){
 //Parse XML file
 var whiteList =["weather.com","google.com","wired.com"];
 
-//////////////////////////////////////////////////////////
-//Only allow websites on the whitelist                  //
-//////////////////////////////////////////////////////////
 function blockOrAllowURL(tab) {
 
 	if (checkWhiteList(tab.url)) {
