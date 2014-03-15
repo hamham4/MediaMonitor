27602cfc1dfcef23c5efc544a3e697acb88a2155
Sara Thorup <thorups@gmail.com>
2014-03-15 15:22:35 -0700

    combined timer, whitelist, and xml reading

--- whiteList/lib/main.js
+++ whiteList/lib/main.js
@@ -1,16 +1,8 @@
-require("sdk/tabs").on("ready", blockOrAllowURL);
-var tmr = require("sdk/timers");
+var whiteList =[];
 var windows = require("sdk/windows").browserWindows;
-
-//pretend that we are readng this from the XML file
-//the time read from the file should be in mins
-var allottedTime =  1;  //in minutes
-
-
-var elapsedTime = 0;   //in minutes
-
-//Every  60 seconds, the timeCheck function is called
-var myTimer = tmr.setInterval(timeCheck, 60000);
+var tmr = require("sdk/timers");
+const {XMLHttpRequest} = require("sdk/net/xhr");
+require("sdk/tabs").on("ready", blockOrAllowURL);
 
 function timeCheck(){
   if (elapsedTime == allottedTime) {
@@ -25,31 +17,49 @@ function timeCheck(){
   elapsedTime = elapsedTime + 1;
 }
 
-//Parse XML file
-var whiteList =["weather.com","google.com","wired.com"];
+var allottedTime =  1;  //in minutes
+var elapsedTime = 0;   //in minutes
+var myTimer = tmr.setInterval(timeCheck, 10000);
 
+var oReq = new XMLHttpRequest();
+oReq.onload = reqListener;
+oReq.open("get", "http://web.engr.oregonstate.edu/~mckinlek/MediaMonitor/configSpecs.xml", true);
+oReq.send();
+
+function reqListener () {
+  var categories =this.responseXML.documentElement.getElementsByTagName("Category");
+  for (i=0;i<categories.length;i++)
+  { 
+    var domains = categories[i].getElementsByTagName("domain");
+    for(j=0; j<domains.length;j++){
+      //console.log(domains[j].childNodes[0].nodeValue);
+      whiteList.push(domains[j].childNodes[0].nodeValue);
+      //console.log(x[i].getElementsByTagName("UserName")[0].childNodes[0].nodeValue);
+    }
+      
+  }
+}
 function blockOrAllowURL(tab) {
 
-	if (checkWhiteList(tab.url)) {
-		 tab.attach({
-    		contentScript: "if (document.body) document.body.style.border = '5px solid green';"
-  		});
-	}
-	else {
-		tab.attach({
-    		contentScript: 'document.body.innerHTML = ' +
-                 ' "<h1>Page not on aproved list</h1>";'
-  		});
-	}
+    if (checkWhiteList(tab.url)) {
+         tab.attach({
+            contentScript: "document.body.style.border = '5px solid green';"
+          });
+
+    }
+    else {
+        tab.attach({
+            contentScript: 'document.body.innerHTML = ' +
+                 ' "<h1>Page not on approved list</h1>";'
+          });
+    }
 }
 
-
 function checkWhiteList(userURL) {
-	var approvedFlag = false;
+    var approvedFlag = false;
     var regexWhiteList = new RegExp(whiteList.join("|"), "i");
     
     approvedFlag = (userURL.match(regexWhiteList) != null);
     
     return approvedFlag;
-
 }
